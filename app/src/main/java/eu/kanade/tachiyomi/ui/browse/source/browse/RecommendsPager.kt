045fc2b4e5c7c19da4378b340844499e31338191
she11sh0cked <she11sh0cked@users.noreply.github.com>
2020-05-18 22:43:28 +0200

    Implement smart recommendations

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/browse/RecommendsPager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/browse/RecommendsPager.kt
@@ -11,6 +11,7 @@ import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SMangaImpl
+import java.util.Locale
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
@@ -19,7 +20,11 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import rx.Observable
 import rx.schedulers.Schedulers
 
-open class RecommendsPager(val manga: Manga, val preferredApi: API = API.MYANIMELIST) : Pager() {
+open class RecommendsPager(
+    val manga: Manga,
+    val smart: Boolean = true,
+    var preferredApi: API = API.MYANIMELIST
+) : Pager() {
     private val client = OkHttpClient.Builder().build()
 
     private fun myAnimeList(): Observable<List<SMangaImpl>>? {
@@ -156,6 +161,23 @@ open class RecommendsPager(val manga: Manga, val preferredApi: API = API.MYANIME
     }
 
     override fun requestNext(): Observable<MangasPage> {
+        if (smart) {
+            val myAnimeListPoints = 0
+            val anilistPoints =
+                anilistSmart.count { manga.genre!!.toLowerCase(Locale.ROOT).contains(it) }
+            val apiPoints = listOf(
+                API.MYANIMELIST to myAnimeListPoints,
+                API.ANILIST to anilistPoints
+            ).sortedWith(
+                compareBy(
+                    { (_, value) -> value },
+                    { (key, _) -> key == preferredApi }
+                )
+            )
+            preferredApi = apiPoints.last().first
+            Log.d("SMART RECOMMEND", preferredApi.toString())
+        }
+
         val apiList = API.values().toMutableList()
         apiList.removeAt(apiList.indexOf(preferredApi))
         apiList.add(0, preferredApi)
@@ -184,6 +206,7 @@ open class RecommendsPager(val manga: Manga, val preferredApi: API = API.MYANIME
     companion object {
         private const val myAnimeListEndpoint = "https://api.jikan.moe/v3/"
         private const val anilistEndpoint = "https://graphql.anilist.co/"
+        private val anilistSmart = listOf("manhua", "manhwa", "webtoon", "long strip")
 
         enum class API { MYANIMELIST, ANILIST }
     }
