af3d147af8b0919e5e4ca88dbdecf6ee92407d25
CrepeTF <trungnguyen02@outlook.com>
2021-02-15 22:48:41 +0000

    Fixed left-handed vertical seekbar animations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         targetSdkVersion(AndroidConfig.targetSdk)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionCode = 13
-        versionName = "r339.3"
+        versionName = "r339.4"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
