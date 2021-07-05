8cdaeb2f8058597c8be5d5726c9241a3814ad1de
simakover <simakover@gmail.com>
2021-07-05 12:52:18 +0500

    Changes from Ru to foreign languages

--- app/src/main/java/exh/smartsearch/SmartSearchEngine.kt
+++ app/src/main/java/exh/smartsearch/SmartSearchEngine.kt
@@ -107,14 +107,20 @@ class SmartSearchEngine(
             cleanedTitle = removeTextInBrackets(preTitle, false)
         }
 
-        // Strip non-special characters
-        cleanedTitle = cleanedTitle.replace(titleRegex, " ")
-
-        // Strip splitters and consecutive spaces
-        cleanedTitle = cleanedTitle.trim().replace(" - ", " ").replace(consecutiveSpacesRegex, " ").trim()
-
         // Strip chapter reference RU
         cleanedTitle = cleanedTitle.replace(chapterRefRuRegexp, " ").trim()
+
+        // Strip non-special characters
+        val cleanedTitleEng = cleanedTitle.replace(titleRegex, " ").trim()
+
+        // Do not strip foreign language letters if cleanedTitle is too short
+        if (cleanedTitleEng.length <= 5) {
+            cleanedTitle = cleanedTitle.replace(titleRegexForeign, " ")
+        } else {
+            cleanedTitle = cleanedTitleEng
+        }
+        // Strip splitters and consecutive spaces
+        cleanedTitle = cleanedTitle.trim().replace(" - ", " ").replace(consecutiveSpacesRegex, " ").trim()
 
         return cleanedTitle
     }
@@ -188,9 +194,10 @@ class SmartSearchEngine(
         const val MIN_SMART_ELIGIBLE_THRESHOLD = 0.4
         const val MIN_NORMAL_ELIGIBLE_THRESHOLD = 0.4
 
-        private val titleRegex = Regex("[^a-zA-Z0-9а-яА-Я- ]")
+        private val titleRegex = Regex("[^a-zA-Z0-9- ]")
+        private val titleRegexForeign = Regex("[^\\p{L}0-9- ]")
         private val consecutiveSpacesRegex = Regex(" +")
-        private val chapterRefRuRegexp = Regex("""((часть|глава) \d*)""")
+        private val chapterRefRuRegexp = Regex("""((- часть|- глава) \d*)""")
     }
 }
 
