a90d51e39c8f8b9878d1628b5001a9753a01f170
Cuong-Tran <cuongtran.tm@gmail.com>
2024-08-22 22:43:52 +0700

    still missing, not sure while it won't report error when running locally

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/ReaderPageImageView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/ReaderPageImageView.kt
@@ -116,7 +116,9 @@ open class ReaderPageImageView @JvmOverloads constructor(
     }
 
     private fun SubsamplingScaleImageView.landscapeZoom(forward: Boolean) {
-        if (config != null && config!!.landscapeZoom && config!!.minimumScaleType == SCALE_TYPE_CENTER_INSIDE &&
+        if (config != null &&
+            config!!.landscapeZoom &&
+            config!!.minimumScaleType == SCALE_TYPE_CENTER_INSIDE &&
             sWidth > sHeight &&
             scale == minScale
         ) {
