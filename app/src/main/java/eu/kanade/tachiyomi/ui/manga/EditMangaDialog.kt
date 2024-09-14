ef9dc9db55826a74f0c26b14e8778801a3c02564
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2024-09-14 12:29:24 +0545

    Show tracker selection dialog when entry has more than one tracker

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/EditMangaDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/EditMangaDialog.kt
@@ -3,20 +3,26 @@ package eu.kanade.tachiyomi.ui.manga
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
@@ -26,7 +32,9 @@ import coil3.transform.RoundedCornersTransformation
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import eu.kanade.presentation.track.components.TrackLogoIcon
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.track.Tracker
 import eu.kanade.tachiyomi.data.track.TrackerManager
 import eu.kanade.tachiyomi.databinding.EditMangaDialogBinding
 import eu.kanade.tachiyomi.source.model.SManga
@@ -42,6 +50,7 @@ import kotlinx.coroutines.launch
 import tachiyomi.core.common.i18n.stringResource
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.track.interactor.GetTracks
+import tachiyomi.domain.track.model.Track
 import tachiyomi.i18n.MR
 import tachiyomi.i18n.sy.SYMR
 import tachiyomi.presentation.core.i18n.stringResource
@@ -67,8 +76,10 @@ fun EditMangaDialog(
     var binding by remember {
         mutableStateOf<EditMangaDialogBinding?>(null)
     }
+    val showTrackerSelectionDialogue = remember { mutableStateOf(false) }
     val getTracks = Injekt.get<GetTracks>()
     val trackerManager = Injekt.get<TrackerManager>()
+    val tracks = remember { mutableStateOf(emptyList<Pair<Track, Tracker>>()) }
     AlertDialog(
         onDismissRequest = onDismissRequest,
         confirmButton = {
@@ -117,7 +128,7 @@ fun EditMangaDialog(
                         EditMangaDialogBinding.inflate(LayoutInflater.from(factoryContext))
                             .also { binding = it }
                             .apply {
-                                onViewCreated(manga, factoryContext, this, scope, getTracks, trackerManager)
+                                onViewCreated(manga, factoryContext, this, scope, getTracks, trackerManager, tracks, showTrackerSelectionDialogue)
                             }
                             .root
                     },
@@ -126,9 +137,61 @@ fun EditMangaDialog(
             }
         },
     )
+
+    if (showTrackerSelectionDialogue.value) {
+        TrackerSelectDialog(
+            tracks = tracks.value,
+            onDismissRequest = { showTrackerSelectionDialogue.value = false },
+            onTrackerSelect = { tracker, track ->
+                scope.launch {
+                    autofillFromTracker(binding!!, track, tracker)
+                }
+            },
+        )
+    }
 }
 
-private fun onViewCreated(manga: Manga, context: Context, binding: EditMangaDialogBinding, scope: CoroutineScope, getTracks: GetTracks, trackerManager: TrackerManager) {
+@Composable
+private fun TrackerSelectDialog(
+    tracks: List<Pair<Track, Tracker>>,
+    onDismissRequest: () -> Unit,
+    onTrackerSelect: (
+        tracker: Tracker,
+        track: Track,
+    ) -> Unit,
+) {
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(),
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(stringResource(MR.strings.action_cancel))
+            }
+        },
+        title = {
+            Text("Select a tracker")
+        },
+        text = {
+            FlowRow(
+                modifier = Modifier
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                tracks.forEach { (track, tracker) ->
+                    TrackLogoIcon(
+                        tracker,
+                        onClick = {
+                            onTrackerSelect(tracker, track)
+                            onDismissRequest()
+                        },
+                    )
+                }
+            }
+        },
+    )
+}
+
+private fun onViewCreated(manga: Manga, context: Context, binding: EditMangaDialogBinding, scope: CoroutineScope, getTracks: GetTracks, trackerManager: TrackerManager, tracks: MutableState<List<Pair<Track, Tracker>>>, showTrackerSelectionDialogue: MutableState<Boolean>) {
     loadCover(manga, binding)
 
     val statusAdapter: ArrayAdapter<String> = ArrayAdapter(
@@ -213,20 +276,30 @@ private fun onViewCreated(manga: Manga, context: Context, binding: EditMangaDial
     binding.resetInfo.setOnClickListener { resetInfo(manga, binding, scope) }
     binding.autofillFromTracker.setOnClickListener {
         scope.launch {
-            autofillFromTracker(manga, binding, getTracks, trackerManager, context)
+            getTrackers(manga, binding, context, getTracks, trackerManager, tracks, showTrackerSelectionDialogue)
         }
     }
 }
 
-private suspend fun autofillFromTracker(manga: Manga, binding: EditMangaDialogBinding, getTracks: GetTracks, trackerManager: TrackerManager, context: Context) {
-    val tracks = getTracks.await(manga.id)
+private suspend fun getTrackers(manga: Manga, binding: EditMangaDialogBinding, context: Context, getTracks: GetTracks, trackerManager: TrackerManager, tracks: MutableState<List<Pair<Track, Tracker>>>, showTrackerSelectionDialogue: MutableState<Boolean>) {
+    tracks.value = getTracks.await(manga.id).map { track ->
+        track to trackerManager.get(track.trackerId)!!
+    }
 
-    if (tracks.isEmpty()) {
+    if (tracks.value.isEmpty()) {
         context.toast("Entry not tracked.")
         return
     }
-    val track = tracks[0]
-    val tracker = trackerManager.get(track.trackerId)!!
+
+    if (tracks.value.size > 1) {
+        showTrackerSelectionDialogue.value = true
+        return
+    }
+
+    autofillFromTracker(binding, tracks.value.first().first, tracks.value.first().second)
+}
+
+private suspend fun autofillFromTracker(binding: EditMangaDialogBinding, track: Track, tracker: Tracker) {
     val trackerMangaMetadata = tracker.getMangaMetadata(track)
 
     binding.title.setText(trackerMangaMetadata.title)
