5095d2ccd1e8b3ca03ee20ede8cfd96b271421fa
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2024-10-28 04:14:10 +0000

    Delete unused Author class.

--- app/src/main/java/eu/kanade/tachiyomi/data/track/mangaupdates/dto/Author.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.data.track.mangaupdates.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Author(
-    val name: String,
-    val type: String,
-)
