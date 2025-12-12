a6bd0bbd2a88e8410493e41e3e1e097ecd278af6
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-12-13 00:49:57 +0600

    Fix reader not saving read duration when changing chapter (#2784)
    (cherry picked from commit 2e0786f699cc6d4863eb20331739c8325a451e63)
    
    # Conflicts:
    #	CHANGELOG.md

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
@@ -467,7 +467,9 @@ class ReaderActivity : BaseActivity() {
     }
 
     override fun onPause() {
-        viewModel.flushReadTimer()
+        lifecycleScope.launchNonCancellable {
+            viewModel.updateHistory()
+        }
         super.onPause()
     }
 
